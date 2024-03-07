34bb0467c8fef2d5971ac6ee5892790ec6d2f944
Shyam Eranky <veranky@ebay.com>
2024-03-07 07:58:55 -0800

    insert players

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,16 @@ insert into POSITION (position) VALUES ('GUARD');
 insert into POSITION (position) VALUES ('CENTER');
 
 insert into TEAM (name,city,coach) VALUES('Golden State Warriors', 'San Francisco', 'Steve Kerr');
-insert into TEAM (name,city,coach) VALUES('Miami Heat', 'Miami', 'Tom');
-insert into TEAM (name,city,coach) VALUES('LA Lakers', 'LA', 'Dick');
+insert into TEAM (name,city,coach) VALUES('LA Lakers', 'LA', 'Tom');
+insert into TEAM (name,city,coach) VALUES('Miami Heat', 'Miami', 'Dick');
 insert into TEAM (name,city,coach) VALUES('Mavericks', 'Dallas', 'Harry');
+
+insert into PLAYER (team_id, name, position) VALUES(1,'Steph Curry', 'GUARD');
+insert into PLAYER (team_id, name, position) VALUES(1,'Draymond Green', 'FORWARD');
+insert into PLAYER (team_id, name, position) VALUES(1,'Klay Thompson', 'GUARD');
+insert into PLAYER (team_id, name, position) VALUES(1,'Iggy', 'CENTER');
+insert into PLAYER (team_id, name, position) VALUES(2,'Lebron James', 'GUARD');
+insert into PLAYER (team_id, name, position) VALUES(2,'Shaq', 'CENTER');
 
 
 
